bf86d1e27e7937b7038132a3001fe61bcd08c0e4
Abhi <husepwarabhi49@gmail.com>
2026-08-11 23:24:02 +0530

    feat: added user repository

--- backend/FoodFrenzy.API/Program.cs
+++ backend/FoodFrenzy.API/Program.cs
@@ -1,16 +1,25 @@
 using FoodFrenzy.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
-
+using FoodFrenzy.Application.Interfaces;
+using FoodFrenzy.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 builder.Services.AddDbContext<FoodFrenzyDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("FoodFrenzyDb")));
 
+
+
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
---
+++ backend/FoodFrenzy.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FoodFrenzy.Application.Interfaces;
+using FoodFrenzy.Domain.Entities;
+using FoodFrenzy.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+namespace FoodFrenzy.Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly FoodFrenzyDbContext _context;
+
+        public UserRepository(FoodFrenzyDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<User?> GetByEmailAsync(string email)
+        {
+            return await _context.Users
+                .FirstOrDefaultAsync(user => user.Email == email);
+        }
+
+        public async Task<User?> GetByIdAsync(Guid id)
+        {
+            return await _context.Users
+                .FirstOrDefaultAsync(user => user.Id == id);
+        }
+
+        public async Task AddAsync(User user)
+        {
+            await _context.Users.AddAsync(user);
+        }
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+    }
+}
